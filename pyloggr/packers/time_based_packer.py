a05c7fceb905edf314485b64d6c582c168d550bc
stephane-martin <stephane.martin_github@vesperal.eu>
2015-04-07 02:58:04 +0200

    packers introduction

--- pyloggr/packers/time_based_packer.py
+++ pyloggr/packers/time_based_packer.py
@@ -10,12 +10,41 @@ __author__ = 'stef'
 from math import fabs
 
 from tornado.gen import coroutine, Return
+from tornado.concurrent import Future
 from spooky_hash import Hash128
+from arrow import Arrow
 
 from pyloggr.event import Event
 
 # merge events when they were emitted in "interval" ms
-interval = 250
+interval_same_event = 250
+
+queue_max_age = 2000
+
+
+class Queue(list):
+    def __init__(self, publisher, exchange, routing_key, persistent):
+        super(Queue, self).__init__()
+        self.exchange = exchange
+        self.routing_key = routing_key
+        self.publisher = publisher
+        self.persistent = persistent
+        self.last_action = Arrow.utcnow()
+
+    @coroutine
+    def publish(self):
+        merged_event = Event.merge(self)
+        # publish the merged event
+        status, ev = yield self.publisher.publish_event(self.exchange, merged_event, self.routing_key, self.persistent)
+        self.last_action = Arrow.utcnow()
+        for event in self:
+            event.have_been_published.set_result(status)
+        self[:] = []
+
+    def append(self, event):
+        super(Queue, self).append(event)
+        event.have_been_published = Future()
+        self.last_action = Arrow.utcnow()
 
 
 class TimeBasedPacker(object):
@@ -60,28 +89,31 @@ class TimeBasedPacker(object):
         key = key.digest()
 
         if key not in self.queues:
-            # that's the first event of its kind: store it and return
-            self.queues[key] = [event]
-            raise Return((None, None))
-        if len(self.queues[key]) == 0:
-            # that's the first event of its kind: store it and return
-            self.queues[key] = [event]
-            raise Return((None, None))
+            self.queues[key] = Queue(self.publisher, exchange, routing_key, persistent)
+        queue = self.queues[key]
+        if len(queue) == 0:
+            # that's the first event of its kind: store it
+            queue.append(event)
+            status = yield event.have_been_published
+            raise Return((status, event))
 
-        first_event = self.queues[key][0]
+        first_event = queue[0]
         diff = event.timereported - first_event.timereported
         diff = fabs(diff.total_seconds()) * 1000
-        if diff < interval:
-            # same series of events
-            self.queues[key].append(event)
-            raise Return((None, None))
+        if diff >= interval_same_event:
+            yield queue.publish()
 
-        # late: the last event is a 'new' event: merge and publish the previous ones
-        merged_event = Event.merge(self.queues[key])
-        # publish the merged event
-        status, ev = yield self.publisher.publish_event(exchange, merged_event, routing_key, persistent)
-        # the initial event is not part of the series, we store it
-        self.queues[key] = [event]
-        raise Return((status, merged_event))
+        queue.append(event)
+        status = yield event.have_been_published
+        raise Return((status, event))
 
-
+    @coroutine
+    def flush(self):
+        """
+        Periodically flush the events stored in Packer queues
+        """
+        now = Arrow.utcnow()
+        for queue in self.queues:
+            age = fabs((now - queue.last_action).total_seconds()) * 1000
+            if age > queue_max_age:
+                yield queue.publish()
