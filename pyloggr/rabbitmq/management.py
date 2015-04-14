001d237005f5feab5cbacc1ca49d69fb681efa3f
stephane-martin <stephane.martin_github@vesperal.eu>
2015-04-14 23:47:13 +0200

    fix

--- pyloggr/rabbitmq/management.py
+++ pyloggr/rabbitmq/management.py
@@ -19,7 +19,7 @@ and/or other materials provided with the distribution.
 
 * Neither the name of the author nor the names of its
 contributors may be used to endorse or promote products derived from this
-software without specific prior written permission.s
+software without specific prior written permission
 """
 
 import logging
