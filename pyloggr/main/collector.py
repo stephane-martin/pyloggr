26cc149c597de046444622d2a3f8910befd9e8b6
stephane-martin <stephane.martin_github@vesperal.eu>
2015-05-26 20:37:43 +0200

    many fixes

--- pyloggr/main/collector.py
+++ pyloggr/main/collector.py
@@ -6,7 +6,7 @@ Collect events from the rescue queue and try to forward them to RabbitMQ
 __author__ = 'stef'
 
 import logging
-from tornado.gen import coroutine
+from tornado.gen import coroutine, TimeoutError
 from tornado.ioloop import IOLoop, PeriodicCallback
 from pyloggr.rabbitmq.publisher import Publisher, RabbitMQConnectionError
 from pyloggr.cache import cache
@@ -18,69 +18,69 @@ logger = logging.getLogger(__name__)
 
 class EventCollector(object):
     def __init__(self, rabbitmq_config):
-        self.rabbitmq_config = rabbitmq_config
-        self.publisher = None
-        self._connect_rabbitmq_later = None
-        self.collecting = False
-        self.shutting_down = False
+        self._rabbitmq_config = rabbitmq_config
+        self._publisher = None
+        self._collecting = False
+        self._shutting_down = False
+        self._periodic_collect = None
 
     @coroutine
-    def start(self):
-        self.publisher = Publisher(self.rabbitmq_config, base_routing_key='pyloggr.syslog.rescue')
+    def launch(self):
+        self._publisher = Publisher(self._rabbitmq_config)
         try:
-            rabbit_close_ev = yield self.publisher.start()
-        except RabbitMQConnectionError:
+            rabbit_close_ev = yield self._publisher.start()
+        except (RabbitMQConnectionError, TimeoutError):
             logger.error("Can't connect to RabbitMQ")
             self.stop()
             yield sleep(Config.SLEEP_TIME)
-            self.connect_later()
+            if not self._shutting_down:
+                IOLoop.instance().add_callback(self.launch)
             return
 
         self._start_periodic()
 
         yield rabbit_close_ev.wait()
         # we lost connection to RabbitMQ
-        self.stop()
+        yield self.stop()
         yield sleep(Config.SLEEP_TIME)
-        self.connect_later()
+        if not self._shutting_down:
+            IOLoop.instance().add_callback(self.launch)
         return
 
-    def connect_later(self):
-        if not self.shutting_down:
-            self._connect_rabbitmq_later = IOLoop.instance().add_callback(self.start)
-
+    @coroutine
     def stop(self):
         self._stop_periodic()
-        if self.publisher:
-            self.publisher.stop()
-            self.publisher = None
+        if self._publisher:
+            yield self._publisher.stop()
+            self._publisher = None
 
+    @coroutine
     def shutdown(self):
-        self.shutting_down = True
-        self.stop()
+        self._shutting_down = True
+        yield self.stop()
 
     def _start_periodic(self):
         """
         Every minute we check if there are some events in the rescue queue
         """
-        if self.periodic_queue_saving is None:
-            self.periodic_queue_saving = PeriodicCallback(
-                callback=self._try_publish_again,
+        if self._periodic_collect is None:
+            self._periodic_collect = PeriodicCallback(
+                callback=self._collect_and_publish,
                 callback_time=Config.SLEEP_TIME * 1000
             )
-            self.periodic_queue_saving.start()
+            self._periodic_collect.start()
 
     def _stop_periodic(self):
         """
         Stop the periodic check
         """
-        if self.periodic_queue_saving:
-            if self.periodic_queue_saving.is_running:
-                self.periodic_queue_saving.stop()
-            self.periodic_queue_saving = None
+        if self._periodic_collect:
+            if self._periodic_collect.is_running:
+                self._periodic_collect.stop()
+            self._periodic_collect = None
 
     @coroutine
-    def _try_publish_again(self):
+    def _collect_and_publish(self):
         """
         _try_publish_again()
         Check the rescue queue, try to publish events in RabbitMQ
@@ -89,28 +89,27 @@ class EventCollector(object):
         ====
         Tornado coroutine
         """
-        if self.collecting:
+        if self._collecting:
             return
-        self.collecting = True
+        self._collecting = True
 
         nb_events = len(cache.rescue)
-        if nb_events is None:
-            logger.info("Rescue queue: can't connect to Redis")
-            return
 
-        if self.publisher is None:
+        if self._publisher is None:
             logger.info("Rescue queue: no connection to RabbitMQ")
+            self._collecting = False
             return
 
         logger.info("{} elements in the rescue queue".format(nb_events))
         if nb_events == 0:
+            self._collecting = False
             return
 
         publish_futures = [
-            self.publisher.publish_event(event, 'pyloggr.syslog.collector') for event in cache.rescue.get_generator()
+            self._publisher.publish_event(event, 'pyloggr.syslog.collector') for event in cache.rescue.event_generator()
         ]
         results = yield publish_futures
         failed_events = [event for (ack, event) in results if not ack]
         map(cache.rescue.append, failed_events)
 
-        self.collecting = False
+        self._collecting = False
