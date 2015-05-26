5df67d9d10f6d046b00eec09f024fdda14573152
stephane-martin <stephane.martin_github@vesperal.eu>
2015-05-26 20:35:38 +0200

    small fixes and perf optims

--- pyloggr/main/shipper2pgsql.py
+++ pyloggr/main/shipper2pgsql.py
@@ -1,4 +1,8 @@
 # encoding: utf-8
+"""
+Ship events to a PostgreSQL database
+"""
+
 __author__ = 'stef'
 
 import logging
@@ -8,28 +12,29 @@ from tornado.ioloop import PeriodicCallback, IOLoop
 # noinspection PyCompatibility
 from concurrent.futures import ThreadPoolExecutor
 from psycopg2.pool import ThreadedConnectionPool, PoolError
+# noinspection PyCompatibility
 import psycopg2
 from sortedcontainers import SortedSet
+from tornado.gen import Return, TimeoutError
 
-from ..utils.constants import SQL_INSERT_QUERY, SQL_COLUMNS_STR, D_COLUMNS
-from ..rabbitmq.consumer import Consumer
-from ..rabbitmq import RabbitMQConnectionError
-from ..event import Event, ParsingError, InvalidSignature
+from pyloggr.utils.constants import SQL_INSERT_QUERY, SQL_COLUMNS_STR, D_COLUMNS
+from pyloggr.rabbitmq.consumer import Consumer, RabbitMQConnectionError
+from pyloggr.event import Event, ParsingError, InvalidSignature
 from pyloggr.utils import sleep
-from pyloggr.utils.simple_queue import Empty
+from pyloggr.config import Config
 
 logger = logging.getLogger(__name__)
 
 
 class PostgresqlShipper(object):
     """
-    PgsqlShipper gets events from RabbitMQ, and inserts them in PostgreSQL
+    PostgresqlShipper retrieves events from RabbitMQ, and inserts them in PostgreSQL
     """
 
     def __init__(self, rabbitmq_config, pgsql_config):
         """
-        :type rabbitmq_config: pyloggr.config.RabbitMQBaseConfig
-        :type pgsql_config: pyloggr.config.PostgresqlConfig
+        :type rabbitmq_config: pyloggr.rabbitmq.Configuration
+        :type pgsql_config: pyloggr.config.Shipper2PGSQL
         """
         self.pgsql_config = pgsql_config
         self.syslog_ev_queue = None
@@ -55,67 +60,83 @@ class PostgresqlShipper(object):
         - Periodically ships the events to PostgreSQL
         """
         self.periodic_check_queue_size = None
+
+        # connect to RabbitMQ
         try:
             closed_conn_event = yield self.consumer.start()
-        except RabbitMQConnectionError:
+        except (RabbitMQConnectionError, TimeoutError):
             logger.error("Can't connect to RabbitMQ")
             yield sleep(60)
             if not self.shutting_down:
                 IOLoop.instance().add_callback(self.launch)
             return
-        yield self._get_db_pool()
+
+        # connect to PGSQL
+        while self.db_pool is None:
+            try:
+                yield self._get_db_pool()
+            except psycopg2.Error:
+                logger.error("shipper: can't connect to PGSQL")
+                yield sleep(Config.SLEEP_TIME)
+
         self.syslog_ev_queue = self.consumer.start_consuming()
-        self.periodic_check_queue_size = PeriodicCallback(
-            self._check_queue_size, callback_time=1000
-        )
+        self.periodic_check_queue_size = PeriodicCallback(self._check_queue_size, callback_time=2000)
         self.periodic_check_queue_size.start()
+
         yield closed_conn_event.wait()
-        # we lost connection to RabbitMQ
-        self.stop()
-        yield sleep(60)
+        # we lost connection to RabbitMQ (by accident, or because stop() was called)
+        yield self.stop()
+        logger.info("Waiting {} seconds before trying to reconnect".format(Config.SLEEP_TIME))
+        yield sleep(Config.SLEEP_TIME)
         if not self.shutting_down:
             IOLoop.instance().add_callback(self.launch)
 
     @coroutine
     def _get_db_pool(self):
-        # we try to connect to PGSQL in a thread, because timeouts can block
-        executor = ThreadPoolExecutor(max_workers=1)
-        try:
-            self.db_pool = yield executor.submit(
-                ThreadedConnectionPool, 1, self.pgsql_config.max_pool, self.dsn, async=False
-            )
-        except:
-            logger.exception("Can't connect to PGSQL")
-            sleep(60)
-            yield self._get_db_pool()
-            return
-        finally:
-            executor.shutdown()
+        if not self.db_pool:
+            # we try to connect to PGSQL in a thread, because connection timeouts can block
+            executor = ThreadPoolExecutor(max_workers=1)
+            try:
+                self.db_pool = yield executor.submit(
+                    ThreadedConnectionPool, 1, self.pgsql_config.max_pool, self.dsn, async=False
+                )
+            finally:
+                executor.shutdown()
+        raise Return(self.db_pool)
 
+    @coroutine
     def stop(self):
         """
         Stops the shipper
         """
+        logger.info("Stopping the shipper2pgsql")
         if self.periodic_check_queue_size:
-            logger.info("Stopping the periodic flush to PGSQL")
             self.periodic_check_queue_size.stop()
             self.periodic_check_queue_size = None
-        self.consumer.stop()
+        if self.consumer:
+            yield self.consumer.stop()
+            self.consumer = None
+        if self.db_pool:
+            if not self.db_pool.closed:
+                self.db_pool.closeall()
+            self.db_pool = None
 
     @coroutine
     def shutdown(self):
         """
         Shutdowns (stops definitely) the shipper.
         """
+        logger.info("Shutting down shipper2pgsql")
         self.shutting_down = True
-        self.stop()
+        yield self.stop()
 
     @coroutine
     def _check_queue_size(self):
-        self._times += 1
-        if self._times >= self.pgsql_config.max_seconds_without_flush or self.syslog_ev_queue.qsize() >= 500:
+        self._times += 2
+        # todo: configurable 500 and 60
+        if self._times >= 60 or self.syslog_ev_queue.qsize() >= 500:
             self._times = 0
-            yield self._flush_messages()
+            IOLoop.instance().add_callback(self._flush_messages)
 
     @coroutine
     def _flush_messages(self):
@@ -127,44 +148,42 @@ class PostgresqlShipper(object):
         logger.info("Flushing events to PGSQL")
         if self.db_pool is None:
             logger.warning("We don't have a pool to PGSQL. Giving up flush. Stopping the consumer.")
-            self.stop()
-            yield sleep(60)
-            yield self.launch()
+            yield self.stop()
             return
         if self.db_pool.closed:
-            self.stop()
-            yield sleep(60)
-            yield self.launch()
+            logger.warning("PGSQL pool is closed. Giving up flush. Stopping the consumer.")
+            yield self.stop()
             return
 
-        try:
-            msgs = self.syslog_ev_queue.get_all_nowait()
-        except Empty:
+        # get_all_nowait pops all the events in syslog_ev_queue
+        msgs = self.syslog_ev_queue.get_all()
+        if not msgs:
             return
 
         logger.info("{} events to forward to PGSQL".format(len(msgs)))
 
-        def flush_backthread(rabbitmq_messages, tablename):
+        def _flush_backthread(rabbitmq_messages, tablename):
+            # parse the bytes messages into real events
+            # we use a SortedSet to get rid of duplicates
             events = SortedSet()
             for rabbit_message in rabbitmq_messages:
                 try:
                     ev = Event.parse_bytes_to_event(rabbit_message.body, hmac=True, json=True)
                 except ParsingError:
                     # should not happen, messages are coming from pyloggr
-                    logger.info("Dropping one message after parsing error")
+                    logger.info("shipper: dropping one unparsable message")
                 except InvalidSignature:
                     security_logger = logging.getLogger('security')
-                    logger.critical("Dropping one tampered event, see security logs")
+                    logger.error("Dropping one tampered event, see security logs")
                     security_logger.critical("Dropping one tampered event")
                     security_logger.critical(rabbit_message.body)
                 else:
                     events.add(ev)
-
             try:
                 conn = self.db_pool.getconn()
-            except PoolError as ex:
-                logging.exception("Can't get a PGSQL connection from the pool")
-                raise ex
+            except PoolError:
+                logging.exception("flush_backthread: can't get a PGSQL connection from the pool")
+                raise
             try:
                 conn.autocommit = False
                 with conn.cursor() as cur:
@@ -176,9 +195,9 @@ class PostgresqlShipper(object):
                     )
                     cur.execute(query)
                     conn.commit()
-            except psycopg2.Error as ex:
-                logger.exception("Flushing to PGSQL failed")
-                raise ex
+            except psycopg2.Error:
+                logger.exception("flush_backthread: flushing to PGSQL failed")
+                raise
             finally:
                 if conn:
                     self.db_pool.putconn(conn)
@@ -186,17 +205,18 @@ class PostgresqlShipper(object):
         executor = ThreadPoolExecutor(max_workers=1)
         try:
             yield executor.submit(
-                flush_backthread, rabbitmq_messages=msgs, tablename=self.pgsql_config.tablename
+                _flush_backthread, rabbitmq_messages=msgs, tablename=self.pgsql_config.tablename
             )
+        except (psycopg2.Error, PoolError):
+            logger.exception("Flushing to PGSQL failed (probably PGSQL connection problem)")
+            map(lambda message: message.nack(), msgs)
+            yield self.stop()
         except:
-            logger.exception("Flushing to PGSQL failed")
-            for msg in msgs:
-                msg.nack()
-            self.stop()
-            yield sleep(60)
-            yield self.launch()
+            logger.error("Admin should review this")
+            logger.exception("shipper: unexpected Exception while flushing events to PGSQL")
+            map(lambda message: message.nack(), msgs)
+            yield self.stop()
         else:
-            for msg in msgs:
-                msg.ack()
+            map(lambda message: message.ack(), msgs)
         finally:
             executor.shutdown()
