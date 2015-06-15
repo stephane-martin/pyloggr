b94d16eca3f3ab45c49d1902fb6eb290a1038925
stephane-martin <stephane.martin_github@vesperal.eu>
2015-06-15 18:05:03 +0200

    bump version to 0.3

--- pyloggr/__init__.py
+++ pyloggr/__init__.py
@@ -3,9 +3,6 @@
 Base package for all pyloggr stuff
 """
 
-# todo: compatibility with python 3.4
-
 __author__ = 'Stephane Martin'
 __email__ = 'stephane.martin_github@vesperal.eu'
-__version__ = '0.1.3'
-
+__version__ = '0.3'
