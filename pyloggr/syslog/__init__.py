03a7635d53600944284b8a162731f3983138059f
stephane-martin <stephane.martin_github@vesperal.eu>
2015-06-15 18:02:41 +0200

    init

--- pyloggr/syslog/__init__.py
+++ pyloggr/syslog/__init__.py
@@ -11,3 +11,13 @@ __author__ = 'stef'
 from .relp_client import RELPClient
 from .tcp_syslog_client import SyslogClient
 from server import BaseSyslogClientConnection, BaseSyslogServer
+
+def client_factory(servr, port, protocol, use_ssl=False, verify_cert=True, hostname=None, ca_certs=None, client_key=None,
+                   client_cert=None, server_deadline=120):
+
+    protocol = str(protocol).lower()
+    if protocol == "relp":
+        return RELPClient(servr, port, use_ssl, verify_cert, hostname, ca_certs, client_key, client_cert, server_deadline)
+    elif protocol == "tcp" or protocol == 'syslog':
+        return SyslogClient(servr, port, use_ssl, verify_cert, hostname, ca_certs, client_key, client_cert, server_deadline)
+
